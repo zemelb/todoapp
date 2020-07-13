d4efb6e740988470c44127079bd153ca290a9f12
zemelb <64750794+zemelb@users.noreply.github.com>
2020-07-13 11:26:47 -0400

    Create app.py
    app for todo app

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    # the description comes to us as a dictionary with the key "description"
+    description = request.get_json()['description']
+
+    #todo variable = Todo table's description field, which we link to the description variable
+    todo = Todo(description=description)
+    
+    #add and commit the description to the table
+    db.session.add(todo)
+    db.session.commit()
+
+    return jsonify({
+        'description': todo.description
+    })
