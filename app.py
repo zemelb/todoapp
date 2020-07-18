4798bbb2a358458659d79d69900668048ce6523b
zemelb <64750794+zemelb@users.noreply.github.com>
2020-07-18 17:07:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 import sysconfig
+import sys 
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -10,23 +11,33 @@ db = SQLAlchemy(app)
 # sets up migration
 migrate = Migrate(app, db)
 
-
+# parent model
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    # sets foreign key to link to todolists table
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+# child model
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    # sets name of the child
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
 
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
-def set_completed_todo(todo_ID):
+def set_completed_todo(todo_id):
     try:
         completed = request.get_json()['completed']
         print('completed', completed)
-        todo = Todo.query.get(todo_ID)
+        todo = Todo.query.get(todo_id)
         todo.completed = completed
         db.session.commit()
     except:
@@ -44,12 +55,19 @@ def create_todo():
         # the description comes to us as a dictionary with the key "description"
         description = request.get_json()['description']
 
+        list_id = request.get_json()['list_id]']
+
         #todo variable = Todo table's description field, which we link to the description variable
-        todo = Todo(description=description)
+        todo = Todo(
+            description=description,
+            completed=False,
+            list_id=list_id)
         
         #add and commit the description to the table
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
         body['description'] = todo.description
 
         return jsonify({
@@ -66,6 +84,17 @@ def create_todo():
         abort (400)
     else:
         return jsonify(body)
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+  try:
+    Todo.query.filter_by(id=todo_id).delete()
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return jsonify({ 'success': True })
 
 @app.route('/')
 def index():
